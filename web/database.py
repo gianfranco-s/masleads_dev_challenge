44bbe34b7c6f4d19df6c4ff835704165c75b7798
gsalomone <gianfranco.s@gmail.com>
2023-10-23 01:21:16 -0300

    Use docker compose

--- web/database.py
+++ web/database.py
@@ -1,11 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from os import getenv
+
+IS_DEV = getenv('IS_DEV', 'false').lower() in ('1', 'true')
+
+if IS_DEV:
+    from dotenv import load_dotenv
+    load_dotenv('../docker.env')
+    host = 'localhost'
+else:
+    # Environment variables are loaded by docker-compose.yml
+    host = 'database'  # Name of the db service defined in docker-compose.yml
+
+
 db_credentials = {
-    'username': 'masleads',
-    'password': 'masleads-pwd',
-    'host': 'localhost',
-    'db_name': 'masleads',
+    'username': getenv('POSTGRES_USR'),
+    'password': getenv('POSTGRES_PWD'),
+    'db_name': getenv('POSTGRES_DB'),
+    'host': host,
 }
 db_url = "postgresql://{username}:{password}@{host}/{db_name}".format(**db_credentials)
 
