d1a1807fc49a516b0fbeade51d26836be75e3cbd
gsalomone <gianfranco.s@gmail.com>
2023-10-23 00:49:49 -0300

    Update code to include db

--- README.md
+++ README.md
@@ -1,33 +1,12 @@
-sudo -u postgres psql -c "CREATE ROLE masleads WITH PASSWORD 'masleads-pwd';"
-sudo -u postgres psql -c "ALTER ROLE masleads WITH LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE masleads WITH OWNER masleads;"
+sudo -u postgres psql
 
-sudo -u postgres psql -c "DROP DATABASE masleads;"
+DROP DATABASE masleads;
+DROP ROLE masleads;
 
-sudo -u postgres psql -c "
-CREATE TABLE \"ElementsToProcess\" (
-    \"id\" serial PRIMARY KEY,
-    \"idBulk\" integer NOT NULL,
-    \"retries\" integer,
-    \"status\" integer NOT NULL,
-    \"name\" character varying(100) NOT NULL
-);
-
-CREATE INDEX \"ElementsToProcess_idBulk_IDX\" ON \"ElementsToProcess\" USING btree (\"idBulk\", \"status\");
-CREATE INDEX \"ElementsToProcess_status_IDX\" ON \"ElementsToProcess\" USING btree (\"status\");
-
-INSERT INTO \"ElementsToProcess\" (\"idBulk\", \"retries\", \"status\", \"name\")
-VALUES
-    (1, 0, 20, 'Element 1'),
-    (1, 1, 20, 'Element 2'),
-    (2, 2, 20, 'Element 3'),
-    (2, 0, 20, 'Element 4'),
-    (3, 0, 60, 'Element 5'),
-    (3, 1, 60, 'Element 6'),
-    (4, 2, 60, 'Element 7'),
-    (5, 0, 80, 'Element 8'),
-    (5, 1, 80, 'Element 9'),
-    (6, 0, 100, 'Element 10');
-";
-
-sudo -u postgres psql -c 'SELECT * FROM "ElementsToProcess";'
+CREATE ROLE masleads WITH PASSWORD 'masleads-pwd';
+ALTER ROLE masleads WITH LOGIN;
+CREATE DATABASE masleads WITH OWNER masleads;
+\c masleads
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE "ElementsToProcess" TO masleads;
+GRANT USAGE ON SCHEMA public TO masleads;
+GRANT USAGE, SELECT ON SEQUENCE "ElementsToProcess_id_seq" TO masleads;
--- web/app.py
+++ web/app.py
@@ -1,59 +1,43 @@
 from flask import Flask, jsonify, request
 
+from web.database import Session
+from web.models import ElementsToProcess
+
 app = Flask(__name__)
 
 
 @app.route('/show-elements', methods=['GET'])
 def show_elements():
-    mock_db = ({
-        'id': 1,
-        'idBulk': 1,
-        'retries': 0,
-        'status': 20,
-        'name': 'Element 1',
-    },
-    {
-        'id': 2,
-        'idBulk': 1,
-        'retries': 1,
-        'status': 20,
-        'name': 'Element 2',
-    })
-    return jsonify(mock_db)
+    with Session() as session:
+        elements = session.query(ElementsToProcess).all()
+    
+    result = [el.__str__() for el in elements]
+    return result
 
 
 @app.route('/show-elements-with-default-status', methods=['GET'])
 def show_elements_with_default_status(default_status: int = 60):
-    mock_db = ({
-        'id': 1,
-        'idBulk': 1,
-        'retries': 0,
-        'status': 20,
-        'name': 'Element 1',
-    },
-    {
-        'id': 2,
-        'idBulk': 1,
-        'retries': 1,
-        'status': 60,
-        'name': 'Element 60',
-    })
-
-    result = [el for el in mock_db if el.get('status') == default_status]
-    return jsonify(result)
+    with Session() as session:
+        elements = session.query(ElementsToProcess).filter(ElementsToProcess.status == default_status)
+    
+    result = [el.__str__() for el in elements]
+    return result
 
 
 @app.route('/insert-element', methods=['POST'])
 def insert_element(default_id_bulk: int = 0, default_retries: int = 0):
     data = request.get_json()
     if 'status' in data and 'name' in data:
-        new_element = {
-            'idBulk': data.get('idBulk', default_id_bulk),
-            'retries': data.get('retries', default_retries),
-            'status': data.get('status'),
-            'name': data.get('name'),
-        }
-        # Insert to db
+        new_element = ElementsToProcess(
+            idBulk = data.get('idBulk', default_id_bulk),
+            retries = data.get('retries', default_retries),
+            status = data.get('status'),
+            name = data.get('name'),
+        )
+        
+        with Session() as session:
+            session.add(new_element)
+            session.commit()
 
         response = 'Insert ok'
         response_status = 201
